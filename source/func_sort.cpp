93074a8a34322bd735e17ba169fd3f6502b3f154
pupach <jr.projekctor@gmail.com>
2023-09-18 14:47:37 +0500

    dsf

--- source/func_sort.cpp
+++ source/func_sort.cpp
@@ -70,9 +70,9 @@ len_arr *sort_bubble(len_arr *arr_ptr, int (*f_sort) (len_arr *, len_arr *))
     memcpy(arr_to_sort, (len_arr *)(arr_ptr->arr), sizeof(len_arr *)*(arr_ptr->size_arr)*1);// why 2 ???????
 
 
-    for (int i = 0; i < (arr_ptr->size_arr); i++)
+    for (int i = 0; i < (arr_to_sort->size_arr); i++)
     {
-        len_arr *may = ((len_arr *)(arr_ptr->arr) + i);
+        len_arr *may = ((len_arr *)(arr_to_sort->arr) + i);
         LOG("sort_bubble 3 number %d, str %s\n", i, (char *)(may->arr));
     }
 
@@ -85,7 +85,7 @@ len_arr *sort_bubble(len_arr *arr_ptr, int (*f_sort) (len_arr *, len_arr *))
         for(size_t i = 0; i < (arr_ptr->size_arr - 1); i++)
         {
             LOG("sort_bubble i = %d, size = %d\n", i, arr_ptr->size_arr);
-            LOG("str1-%d, str2-%d", i, arr_ptr->size_arr);
+            LOG("str1-%d, str2-%d", i, i+1);
 
             int res = f_sort((len_arr *)(arr_to_sort)  + i, (len_arr *)(arr_to_sort)  + i+1);
             if (res == -1)
